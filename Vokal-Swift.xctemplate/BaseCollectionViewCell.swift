396de04bf5e95f0bb0f7e5585a09d4bc17b87786
murawaki-村脇 <mitsuhiromurawaki@gmail.com>
2017-08-13 22:21:49 +0900

    Update Pods Option

--- Vokal-Swift.xctemplate/BaseCollectionViewCell.swift
+++ Vokal-Swift.xctemplate/BaseCollectionViewCell.swift
@@ -7,42 +7,16 @@
 //
 
 import UIKit
+import RxSwift
 
-/**
- * Base class for collection view cells. Extend this for any custom cell classes, and
- * override commonInit() to perform cell setup there.
- * Make sure to call super.commonInit() in your implementation!
- */
-class BaseCollectionViewCell: UICollectionViewCell, CommonInitializedView {
+class BaseCollectionViewCell: UICollectionViewCell {
     
-    var oneTimeThingsAreSetUp = false
+    lazy var disposeBag = DisposeBag()
+    let onPrepareForReuse: Observable<Void> = PublishSubject()
     
-    // MARK: - Setup functions which should be overridden
-    
-    func commonInit() {
-        self.setupOneTimeThingsIfNeeded()
-    }
-    
-    func setupOneTimeThings() {
-        self.oneTimeThingsAreSetUp = true
-    }
-    
-    // MARK: - Initialization
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
-    
-    // MARK: - Interface Builder
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        self.commonInit()
-    }
-    
-    override func prepareForInterfaceBuilder() {
-        super.prepareForInterfaceBuilder()
-        self.commonInit()
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        (self.onPrepareForReuse as? PublishSubject<Void>)?.on(.next())
     }
     
 }
